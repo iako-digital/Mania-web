2e824516a471609b7721e536148a74b472c8fe0d
iako-digital <iakodigital@gmail.com>
2026-09-03 11:18:19 +0400

    Fix Azure Functions deployment by enforcing static HTML export to out folder

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: { unoptimized: true },
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
 };
 
 export default withNextIntl(nextConfig);
